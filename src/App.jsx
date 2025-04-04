95f3b111fa6ab768b45f77ed69171cc835f0014e
Puneet <puneetkumardhal@gmail.com>
2025-04-05 03:58:45 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import { LogIn } from 'lucide-react'
+import { LogIn, Router } from 'lucide-react'
 import './App.css'
 import Landing from './pages/Landing'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
 
   return (
     <>
-      <Signup  />
+    
+    <AuthProvider>
+    <Router>
+    <Routes>
+                    <Route path="/" element={<Landing />} />
+                    <Route path="/signin" element={<Signup/>} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/changePassword" element={<ChangePassword />} />
+                    <Route
+                        path="/dashboard/*"
+                        element={
+                            <PrivateRoute>
+                                <Dashboard />
+                            </PrivateRoute>
+                        }
+                    />
+                </Routes>
+    </Router>
+    </AuthProvider>
     </>
   )
 }
